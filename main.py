3a8cf3b9ce1a8173b15d1e1624537d62d9e965a2
folashade olanrewaju <112771723+fola2022@users.noreply.github.com>
2023-01-04 12:54:28 +0000

    Tip Calculator

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-#1. Create a greeting for your program.
-print("Welcome to Oslo band name generator")
-#2. Ask the user for the city that they grew up in.
-city = input("Which city did you grew up in?\n")
-#3. Ask the user for the name of a pet.
-pet = input("what is the name of your pet?\n")
-#4. Combine the name of their city and pet and show them their band name.
-print("Your band name could be " + city + " " + pet)
-#5. Make sure the input cursor shows on a new line:
+#If the bill was $150.00, split between 5 people, with 12% tip. 
 
-# Solution: https://replit.com/@appbrewery/band-name-generator-end
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+print("Welcome to the tip calculator!")
+bill = float(input("What is the total bill? $ "))
+tip = int(input("How much tip would you like to give 10, 12 or 15? "))
+people = int(input("How many people are spliting the bill? "))
+bill_with_tip = tip / 100 * bill + bill
+print(bill_with_tip)
+bill_per_person = bill_with_tip / people
+final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
+print(f"Each per should pay ${final_amount} ")
